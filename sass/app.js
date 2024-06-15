a25acca7e0e1f6cef7f1c7ec27cdd1f8ed3e367f
ronakbhutiya <ronakbhutiya123@gmail.com>
2024-06-15 18:00:12 +0530

    add sticky nav

--- sass/app.js
+++ sass/app.js
@@ -3,3 +3,19 @@ const nav__btn = document.querySelector(".navigation__btn");
 nav__btn.addEventListener("click", function () {
   nav__header.classList.toggle("active");
 });
+
+const header = document.querySelector(".header");
+const observer = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+    ent.isIntersecting === false
+      ? nav__header.classList.add("sticky")
+      : nav__header.classList.remove("sticky");
+  },
+  {
+    root: null,
+    rootMargin: "",
+    threshold: "",
+  }
+);
+observer.observe(header);
